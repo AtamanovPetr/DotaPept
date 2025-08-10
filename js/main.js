abe4a4b730000c0a96e68a3b9098505d1566057e
Peter <peteratamanov@yandex.ru>
2025-08-10 14:29:01 +0300

    fix: header position

---
+++ js/main.js
@@ -0,0 +1,8 @@
+const navbar = document.querySelector('.main-header');
+window.addEventListener('scroll', () => {
+  if (this.scrollY > 1) {
+    console.log('Прокрутка');
+  } else {
+    
+  }
+})
